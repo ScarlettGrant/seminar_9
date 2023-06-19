aef5931960c38487e339e8474cda7c5d5b82fadb
Scarlett <sharashova.maria@yandex.ru>
2023-06-19 14:18:25 +0400

    solution 2

---
+++ solution_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных чисел в промежутке от M до N.
+
+int SumInRow(int m, int n)
+{
+    if (m == n) return m;
+    else return n + SumInRow(m, n - 1); 
+}
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SumInRow(number1, number2));
